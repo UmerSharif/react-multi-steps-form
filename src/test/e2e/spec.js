ab73786e40dba37dd75ab475411f63cc17ae2b9e
Bill Huang <billonline33@hotmail.com>
2021-03-16 14:28:06 +1100

    Feature/e2e: first real test case
    1. add utils.ts helper
    2. modify spec.js to test title

--- src/test/e2e/spec.js
+++ src/test/e2e/spec.js
@@ -1,6 +1,11 @@
-describe('Protractor Demo App', function () {
-  it('should have a title', function () {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    expect(browser.getTitle()).toEqual('Super Calculator');
+import { browser, element, by } from 'protractor';
+import SignInPage from './page-objects/signin-page';
+import { waitUntilDisplayed, getToastByInnerText } from './util/utils';
+
+describe('Registration Form e2e Test', function () {
+  it('should have a title', async () => {
+    browser.waitForAngularEnabled(false);
+    browser.get(browser.baseAppUrl);
+    expect(browser.getTitle()).toEqual('Multi Steps Online Registration');
   });
 });
